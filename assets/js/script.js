64d6734aa9a90708c2fa81e9f9e9e6135e2b0688
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-05-06 20:45:40 +0000

    added boxes to tell player hwo to play the game. the should appear on pressing how to, and disapear on starting game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 $('#tutorial').click(function() {
     $('#how-to').html("<img src='assets/images/CodeBreakerPlay.png' alt='code breaker game' role='presentation'>Code Breaker</img>");
+    $('#how-to-one').html("<p>Click the nodes to change their colours.</p>");
+    $('#how-to-two').html("<p>6 colours to choose from. That's 1296 possible codes!</p>");
+    $('#how-to-three').html("<p>Correct nodes light up green. Can you break the code in 8 tries?!</p>");
 });
 
 $('#start-game').click(runGame);
 var colorArray = ['red','yellow','orange','green','blue','purple']; // colorArray idea given by igor_ci on interactive-frontend board of slack.
+
 var secretCode = [];
 for (let i=0; i < 4; i++) {
     x = Math.floor(Math.random() * 5);
@@ -24,6 +28,10 @@ var dr = 0;
 //1=red, 2=yellow, 3=orange, 4=green, 5=blue, 0=purple
 //then calls roundOne and makes breaker button visible.
 function runGame() {
+    $('#how-to').html("");
+    $('#how-to-one').html("");
+    $('#how-to-two').html("");
+    $('#how-to-three').html("");
     $('#game-container').html('<table id="gameBoard"><tr><th>A</th><th>B</th><th>C</th><th>D</th></tr><tr><td><div class="gamePeg peg1" id="a1"></div></td><td><div class="gamePeg peg1" id="b1"></div></td><td><div class="gamePeg peg1" id="c1"></div></td><td><div class="gamePeg peg1" id="d1"></div></td></tr><tr><td><div class="gamePeg peg2" id="a2"></div></td><td><div class="gamePeg peg2" id="b2"></div></td><td><div class="gamePeg peg2" id="c2"></div></td><td><div class="gamePeg peg2" id="d2"></div></td></tr><tr><td><div class="gamePeg" id="a3"></div></td><td><div class="gamePeg" id="b3"></div></td><td><div class="gamePeg" id="c3"></div></td><td><div class="gamePeg" id="d3"></div></td></tr><tr><td><div class="gamePeg" id="a4"></div></td><td><div class="gamePeg" id="b4"></div></td><td><div class="gamePeg" id="c4"></div></td><td><div class="gamePeg" id="d4"></div></td></tr><tr><td><div class="gamePeg" id="a5"></div></td><td><div class="gamePeg" id="b5"></div></td><td><div class="gamePeg" id="c5"></div></td><td><div class="gamePeg" id="d5"></div></td></tr><tr><td><div class="gamePeg" id="a6"></div></td><td><div class="gamePeg" id="b6"></div></td><td><div class="gamePeg" id="c6"></div></td><td><div class="gamePeg" id="d6"></div></td></tr><tr><td><div class="gamePeg" id="a7"></div></td><td><div class="gamePeg" id="b7"></div></td><td><div class="gamePeg" id="c7"></div></td><td><div class="gamePeg" id="d7"></div></td></tr><tr><td><div class="gamePeg" id="a8"></div></td><td><div class="gamePeg" id="b8"></div></td><td><div class="gamePeg" id="c8"></div></td><td><div class="gamePeg" id="d8"></div></td></tr></table>');
     roundOne();
     $('.breaker').css('visibility', 'visible');
